0fdc4a4fb5be47f6c309bbd52d0e260e1c118141
Your Name <107945487+KoliaDemianovych@users.noreply.github.com>
2025-02-27 19:34:22 +0200

    rm. /base

--- taskboard/base/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Task, User, Admin
-
-admin.site.register(Task)
-admin.site.register(User)
-admin.site.register(Admin)
--- taskboard/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TaskListCreate, TaskDetail, UserList
-
-urlpatterns = [
-    path('tasks/', TaskListCreate.as_view(), name='task-list-create'),
-    path('tasks/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
-    path('users/', UserList.as_view(), name='user-list'),
-]
